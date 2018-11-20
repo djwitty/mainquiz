e0f54ca7d158b57ed365407ff64a8b6e6f13e8cf
Mikhail Pov <mikhail@appscloud.io>
2018-11-20 17:04:49 +0200

    modified:   index.html 	modified:   src/js/index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,13 @@
 // /getUpdates
 */
 
-console.log(window);
+const BASE_URL = 'https://api.telegram.org/bot';
+const TOKEN = '706728723:AAE4EboBhbOWw4i0GhecG3mbUDCh38Wb2yU';
+var method = 'getUpdates';
+
+var url = BASE_URL + TOKEN + '/' + method;
+var link = '';
+var quizBotLink = document.getElementById('quizBotLink');
+quizBotLink.addEventListener("click", function(e) {
+  e.target.href = url;
+}, false);
